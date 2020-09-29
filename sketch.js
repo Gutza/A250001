c347f07a0af6351c6f096f4588e95b71093de16e
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2020-09-29 19:26:26 +0300

    Started experimenting with HHogg's algorithm

--- sketch.js
+++ sketch.js
@@ -15,8 +15,6 @@ class Circle {
 		this.x = x;
 		this.y = y;
 		this.r = 100;
-		// Interesting points on this circle
-		this.intersections = [];
 		// Original X and Y; used when dragging
 		this.ox = x;
 		this.oy = y;
@@ -31,96 +29,12 @@ class Circle {
 			this.p.strokeWeight(1);
 			this.p.circle(this.x, this.y, this.r * 2); // p5js uses the diameter
 		}
-		
-		if (gp.ShowIntersections || gp.ShowRadicalLines) {
-			this.p.fill(this.p.color(255, 255, 255));
-			for (let iIndex in this.intersections) {
-				let i = this.intersections[iIndex];
-				let i1 = this.index;
-				let i2 = iIndex;
-				if (i1 < i2) {
-					return;
-				}
-				
-				if (gp.ShowRadicalLines) {
-					this.p.stroke(this.p.color(255, 255, 255));
-					this.p.strokeWeight(4);
-					this.p.line(i.int1.x, i.int1.y, i.int2.x, i.int2.y);
-					
-					this.p.stroke(this.p.color((i1*83+i2*77)%256, (i1*39+i2*11)%256, (i1*41+i2*13)%256));
-					this.p.strokeWeight(2);
-					this.p.line(i.int1.x, i.int1.y, i.int2.x, i.int2.y);
-				}
-								
-				if (gp.ShowIntersections) {
-					this.p.strokeWeight(1);
-					this.p.stroke(this.oc);
-					this.p.circle(i.int1.x, i.int1.y, 5);
-					this.p.circle(i.int2.x, i.int2.y, 5);
-				}
-			}
-		}
-	}
-
-	relationTo = c => {
-		let centerDist = this.p.dist(this.x, this.y, c.x, c.y);
-		if (centerDist > this.r + c.r) {
-			return {
-				rel: "disjunct"
-			};
-		}
-
-		if (centerDist + this.r < c.r) {
-			return {
-				rel: "this in that"
-			};
-		}
-
-		if (centerDist + c.r < this.r) {
-			return {
-				rel: "that in this"
-			};
-		}
-
-		// Find a and h
-		let a = (this.r * this.r - c.r * c.r + centerDist * centerDist) / (2 * centerDist);
-		let h = Math.sqrt(this.r * this.r - a * a);
-
-		// Find P2
-		let cx2 = this.x + a * (c.x - this.x) / centerDist;
-		let cy2 = this.y + a * (c.y - this.y) / centerDist;
-
-		let result = {
-			rel: "intersection",
-			int1: this.p.createVector(
-				cx2 + h * (c.y - this.y) / centerDist,
-				cy2 - h * (c.x - this.x) / centerDist
-			),
-			int2: this.p.createVector(
-				cx2 - h * (c.y - this.y) / centerDist,
-				cy2 + h * (c.x - this.x) / centerDist
-			)
-		};
-		this.intersections[c.index] = {
-			int1: result.int1,
-			int2: result.int2,
-		};
-		c.intersections[this.index] = {
-			int1: result.int1,
-			int2: result.int2,
-		};
-		radicalLines[this.index.toString()+"-"+c.index.toString()] = [[ result.int1.x, result.int1.y ], [ result.int2.x, result.int2.y]];
-
-		return result;
 	}
 }
 
 const s = (sketch) => {
 	var guiParams = {
 		ShowCircles: true,
-		ShowIntersections: false,
-		ShowRadicalLines: true,
-		ShowRadicalIntersections: true,
 	}
 	var gui;
 
@@ -141,11 +55,9 @@ const s = (sketch) => {
 		circles.forEach(c => {
 			c.draw(guiParams);
 		});
-		if (guiParams.ShowRadicalIntersections) {
-			radicalIntersections.forEach(ri => {
-				sketch.circle(ri.x, ri.y, 5);
-			});
-		}
+		interestingPoints.forEach(p=>{sketch.circle(p.x, p.y, 3)});
+		
+		sketch.noLoop();
 	}
 
 	let candidates = [];
@@ -167,6 +79,7 @@ const s = (sketch) => {
 			c.c = sketch.color(255, 0, 0);
 			candidates.push(c);
 		});
+		sketch.loop();
 	}
 
 	let startX = -1;
@@ -179,6 +92,7 @@ const s = (sketch) => {
 		}
 		startX = sketch.mouseX;
 		startY = sketch.mouseY;
+		sketch.loop();
 	}
 
 	sketch.validMouseTarget = event => {
@@ -231,29 +145,32 @@ const s = (sketch) => {
 	this.delayHandle = null;
 
 	sketch.refreshTable = () => {
+		sketch.loop();
 		clearTimeout(this.delayHandle);
 		this.delayHandle = setTimeout(sketch._refreshTable, 20);
 	}
 	
+	let interestingPoints = [];
 	sketch._refreshTable = () => {
-		radicalLines = {};
-		circles.forEach(cThis => {
-			cThis.intersections = [];
+		interestingPoints = [];
+		let hhCircles = [];
+		circles.forEach(c => {
+			hhCircles.push(new circleRegions.circle({radius: c.r, x: c.x, y: c.y}, c.index));
 		});
-		
-		circles.forEach(cThis => {
-			circles.forEach(cThat => {
-				if (cThis.index >= cThat.index) {
-					return;
-				}
-				let r = cThis.relationTo(cThat);
-				if (r.rel == "disjunct") {
-					return;
-				}
+		let hhResult = circleRegions.getIntersectionAreas(hhCircles);
+		hhResult.areas.forEach(a => {
+			if (a.isCircle) {
+				return;
+			}
+			
+			a.arcs.forEach(a=>{
+				interestingPoints.push({x: a.mx, y: a.my});
 			});
 		});
-		radicalIntersections = findIntersections(radicalLines);
+		console.log("hhResult", hhResult);
+		sketch.loop();
 	}
+	
 }
 
 let myp5 = new p5(s);
